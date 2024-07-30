27cb90727a59a1d1c00a0666844735468df1280e
yuxingliang02 <yuxingliang02@meituan.com>
2024-07-30 23:23:37 +0800

    feat: heads/master-0-g6618b65

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
+
+# 判断是否传入参数
+if [ "$1" = "win" ]; then
+  build_cmd="build:win"
+else
+  build_cmd="build"
+fi
 
 push_addr=https://github.com/YuXingLiang214/leonBlog.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
@@ -9,17 +15,15 @@ dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 push_branch=gh-pages # 推送的分支
 
 # 生成静态文件
-npm run build
+npm run $build_cmd
 
 # 进入生成的文件夹
 cd $dist_path
-
 git init
 git add -A
 git commit -m "deploy, $commit_info"
 git remote add origin $push_addr
 git branch -M master
 git push -f $push_addr HEAD:$push_branch
-
 cd -
 rm -rf $dist_path
